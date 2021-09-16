6d9cb8fd39d3f142e8f4a890f5f2b3a7b55ce58b
VasuTalend <35964890+VasuTalend@users.noreply.github.com>
2021-09-16 15:08:45 +0530

    commited

--- sp_dim_group.sql
+++ sp_dim_group.sql
@@ -1,6 +1,6 @@
 --Modified comments2
 
-CREATE OR REPLACE PROCEDURE edw.sp_dim_groupMODIFIED2(in_src_last_modified "TIMESTAMP",in_run_id "int4",in_job_name "varchar") 
+CREATE OR REPLACE PROCEDURE edw.sp_dim_groupMODIFIED3(in_src_last_modified "TIMESTAMP",in_run_id "int4",in_job_name "varchar") 
 LANGUAGE plpgsql AS $$
 
 DECLARE
